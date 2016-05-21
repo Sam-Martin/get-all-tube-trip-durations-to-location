d477fbd0f01f36da03626910a5d56537b993d5fa
Sam Martin <samjackmartin@gmail.com>
2016-05-21 01:58:51 +0100

    Merge in partial results to existing records

--- serverless-london-travel-times/functions/set-traveltimes/handler.js
+++ serverless-london-travel-times/functions/set-traveltimes/handler.js
@@ -16,36 +16,55 @@ module.exports.handler = function(event, context, cb) {
   };
 
   docClient.get(params, function(err, data) {
-      if (err) {
-        return cb(null, {
-          Error: "Unable to create item. Error JSON:" + JSON.stringify(err, null, 2)
-        });
-
-      } else if (!Object.keys(data).length){
-        // If this desintation does not already exist, go ahead and add it!
-        var params = {
-            TableName: table,
-            Item:{
-                "destination": event.destination,
-                origins: event.origins
-            }
-        };
-
-        docClient.put(params, function(err, data) {
-            if (err) {
-              return cb(null, {
-                Error: "Unable to add item. Error JSON:" + JSON.stringify(err, null, 2)
-              });
-
-            } else {
-              return cb(null, {message: "Successfully added item"});
-
-            }
-        });
-    } else {
+    if (err) {
       return cb(null, {
-        Error: "Unable to create item. It already exists."
+        Error: "Unable to create item. Error JSON:" + JSON.stringify(err, null, 2)
       });
+
+
+
+    } else {
+
+      if (Object.keys(data).length){
+        // if this destination already exists, merge the values
+        origins = data.Item.origins
+        message = "Successfully merged item"
+      }else{
+        // If this destination does not already exist, go ahead and add it!
+        origins = {}
+        message = "Successfully added item"
+      }
+
+      // Merge/Strip out empty origins
+      for(var originName in event.origins){
+        console.log("evaluating " + originName)
+        if(Object.keys(event.origins[originName]).length){
+          console.log("adding " + originName)
+          origins[originName] = event.origins[originName]
+        }
+      }
+
+
+      var params = {
+          TableName: table,
+          Item:{
+              "destination": event.destination,
+              origins: origins
+          }
+      };
+
+      docClient.put(params, function(err, data) {
+          if (err) {
+            return cb(null, {
+              Error: "Unable to add item. Error JSON:" + JSON.stringify(err, null, 2)
+            });
+
+          } else {
+            return cb(null, {message: message});
+
+          }
+      });
+
     }
   });
 };
